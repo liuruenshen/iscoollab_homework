1746041714455f05e4066eb71caf2a3ab80ec029
kiwi <kiwi71728@icloud.com>
2022-01-08 06:49:33 +0800

    refactor: set up redux store

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { render } from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import Order from "../Order/Order";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Order from '../Order/Order';
+import { Provider } from 'react-redux';
+import { store } from '../../redux/store';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Order />}>
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Order />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   rootElement
 );
