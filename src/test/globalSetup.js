b2285455c0bcabdc6ca7eec12541eba90bba392b
kiwi <kiwi71728@icloud.com>
2022-01-07 08:32:01 +0800

    fix: detach child process and kill by child process group id

--- src/test/globalSetup.js
+++ src/test/globalSetup.js
@@ -1,16 +1,17 @@
 const { spawn } = require('child_process');
+const { kill } = require('process');
 
 let apiServer = null;
 module.exports = () => {
   if (apiServer) {
-    apiServer.kill(9);
-    return Promise.resolve(true);
+    kill(`-${apiServer.pid}`, 'SIGKILL');
+    return Promise.resolve();
   } else {
     let localResolve = null;
     const promise = new Promise((resolve) => {
       localResolve = resolve;
     });
-    apiServer = spawn('yarn', ['watch:server']);
+    apiServer = spawn('yarn', ['watch:server'], { detached: true });
     apiServer.stdout.on('data', (data) => {
       console.log('[API SERVER][stdout]', data.toString());
       localResolve();
